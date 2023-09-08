7aec4819e955bff4f2bac2eca6b03327ffb74222
InfiniteVerma <vermaanant25@gmail.com>
2023-09-08 20:28:14 +0530

    sync changes

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Steps:
 
  - Clone the repo
  - `./script.sh`
+
+Config things
+
+1. In firefox, enter "about:config" and make `browser.fullscreen.autohide` as `false`
--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 # symlinking
 echo "Symlinking stuff to .config"
 
+rm -rf ~/.config/rofi
+rm -rf ~/.config/hypr
+rm -rf ~/.config/waybar
+rm -fr ~/.config/scripts
+
 ln -rs hypr/ ~/.config/
 ln -rs waybar/ ~/.config/
+ln -rs scripts/ ~/.config/
+ln -rs rofi/ ~/.config/
