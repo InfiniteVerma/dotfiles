d80e5499788849b8af7877f219a15af416747dc8
InfiniteVerma <vermaanant25@gmail.com>
2023-09-11 10:08:56 +0530

    sync changes

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ rm -rf ~/.config/rofi
 rm -rf ~/.config/hypr
 rm -rf ~/.config/waybar
 rm -fr ~/.config/scripts
+rm -fr ~/.config/nvim
 
 ln -rs hypr/ ~/.config/
 ln -rs waybar/ ~/.config/
 ln -rs scripts/ ~/.config/
 ln -rs rofi/ ~/.config/
+ln -rs nvim/ ~/.config/
